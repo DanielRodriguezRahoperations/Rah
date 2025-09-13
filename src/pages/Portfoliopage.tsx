4a7235d6c31430e91cabe3b9a25908a0289c6b4f
DanielRodriguezRahoperations <Daniel@pinnaclebookkeepingaz.com>
2025-09-12 20:01:02 -0700

    Update and rename PortfolioPage.tsx to Portfoliopage.tsx

---
+++ src/pages/Portfoliopage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>Portfolio Test Page</h1>
+      <p>This is a test to see if the build works.</p>
+    </div>
+  );
+};
+
+export default PortfolioPage;
